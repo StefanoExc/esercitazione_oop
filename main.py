8f1134d6146da9da885ebe2edb56c960a7549422
StefanoExc <stefanobls7@gmail.com>
2022-01-19 10:32:03 +0100

    Iterazione #1

--- main.py
+++ main.py
@@ -1,26 +1,20 @@
-# How to create a class:
 class Item:
-    def calculate_total_price(self, x, y):
-        return x * y
+    def __init__(self, name:str, price:float, quantity=0):
+        #Chek price and quantity
+        assert price >=0, f"Il prezzo {price} è minore di zero!"
+        assert price >=0, f"La quantità {quantity} è minore di zero!"
 
+        self.name=name
+        self.price=price
+        self.quantity=quantity
+
+    def calculate_total_price(self):
+        return self.price * self.quantity
+    
 # How to create an instance of a class
-item1 = Item()
-
-# Assign attributes:
-item1.name = "Phone"
-item1.price = 100
-item1.quantity = 5
+item1 = Item("Phone",100,5)
+item2 = Item("Laptop",1000,3)
 
 # Calling methods from instances of a class:
-print(item1.calculate_total_price(item1.price, item1.quantity))
-
-# How to create an instance of a class (We could create as much as instances we'd like to)
-item2 = Item()
-
-# Assign attributes
-item2.name = "Laptop"
-item2.price = 1000
-item2.quantity = 3
-
-# Calling methods from instances of a class: 
-print(item2.calculate_total_price(item2.price, item2.quantity))
+print(item1.calculate_total_price())
+print(item2.calculate_total_price())
